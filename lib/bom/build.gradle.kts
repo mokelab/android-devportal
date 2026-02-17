7d7c14fb457260606a79fec04270536878d901bc
Hirokazu Fukami <fkm@mokelab.com>
2026-02-17 10:27:46 +0900

    Add BOM module

---
+++ lib/bom/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java-platform")
+    id("maven-publish")
+}
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    constraints {
+        api(libs.mokelab.devportal.api)
+        api(libs.mokelab.devportal.devportal)
+        api(libs.mokelab.devportal.logcat)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("bom") {
+            from(components["javaPlatform"])
+            groupId = libs.mokelab.devportal.bom.get().group
+            artifactId = libs.mokelab.devportal.bom.get().name
+            version = libs.versions.devportal.get()
+        }
+    }
+    repositories {
+        maven {
+            url = uri("${rootProject.rootDir.absolutePath}/repo")
+        }
+    }
+}
