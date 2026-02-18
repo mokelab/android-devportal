72645e398f738717f6bd139f74f58b50d44dfadd
Hirokazu Fukami <fkm@mokelab.com>
2026-02-18 15:35:23 +0900

    Update repository path in settings to point to docs/repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven {
-            url = uri("${rootProject.projectDir.absolutePath}/repo")
+            url = uri("${rootProject.projectDir.absolutePath}/docs/repo")
         }
     }
 }
